551bb1f26e181d42a061e571270403ceb52e289b
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2023-11-23 19:37:34 -0800

    Play around with basic `object` type validations

--- zod/src/main.ts
+++ zod/src/main.ts
@@ -1,13 +1,23 @@
 import { z } from 'zod'
 
+const themes = ['LIGHT', 'DARK'] as const
+
 const UserSchema = z.object({
-  username: z.string()
+  username: z.string().min(3).max(20),
+  age: z.number().gt(0).optional(),
+  email: z.string().email(),
+  isAdmin: z.boolean().default(false),
+  theme: z.enum(themes).default('DARK')
 })
 
 type User = z.infer<typeof UserSchema>
 
 const user: User = {
-  username: 'edwardshturman'
+  username: 'edwardshturman',
+  age: 20,
+  email: 'emshturman@dons.usfca.edu',
+  isAdmin: true,
+  theme: 'DARK'
 }
 
 console.log(UserSchema.parse(user))
