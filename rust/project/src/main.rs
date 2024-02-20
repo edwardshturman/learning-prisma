ec6b32b42c033962f7eb1a7cc06114b25510ed4e
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2024-02-19 20:09:09 -0800

    Add basic Rust from workshop

---
+++ rust/project/src/main.rs
@@ -0,0 +1,12 @@
+fn new(input: i32) -> i32 {
+    return 42;
+}
+
+fn main() {
+    let foo = 42;
+    let bar: i32 = 42;
+    let mut mutable = 42;
+
+    String::new();
+    println!("Hello, world!");
+}
